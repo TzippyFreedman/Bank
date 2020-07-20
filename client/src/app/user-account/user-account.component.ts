75d227638f8e99be17d221916d78d3aaa73b552f
estherRivka <estherivka1@gmail.com>
2020-07-20 22:50:31 +0300

    added logout

--- client/src/app/user-account/user-account.component.ts
+++ client/src/app/user-account/user-account.component.ts
@@ -1,35 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IUser } from '../shared/models/IUser';
 import { DataService } from '../shared/services/data.service';
-import { cwd } from 'process';
 import { IAccount } from '../shared/models/IAccount';
 import { CookieService } from 'ngx-cookie-service';
+
 @Component({
   selector: 'app-user-account',
   templateUrl: './user-account.component.html',
   styleUrls: ['./user-account.component.css']
 })
 export class UserAccountComponent implements OnInit {
+
   account: IAccount;
   accountId: string;
-  constructor(private route:ActivatedRoute, private http: DataService,private cookieService: CookieService) { }
 
-  
+  constructor(private route: ActivatedRoute, private http: DataService, private cookieService: CookieService) { }
+
   ngOnInit(): void {
-    this.route.paramMap.subscribe( paramMap => {
+    this.route.paramMap.subscribe(paramMap => {
       this.accountId = paramMap.get('userFileId');
       //this.cookieService.set( 'userAccountId',  this.accountId );
-  });
-
-  this.http.getAccountDetails(this.accountId)
-  .subscribe(
-    result => {
-    this.account = result; },
-        error => {
-        alert(error);
-
     });
+
+    this.http.getAccountDetails(this.accountId)
+      .subscribe(
+        result => {
+          this.account = result;
+        },
+        error => {
+          alert(error);
+        });
   }
 
 
