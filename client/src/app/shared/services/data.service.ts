a6dcbfeb6e1c97cd56a7f72c309e2974f007f760
estherRivka <estherivka1@gmail.com>
2020-07-23 17:59:31 +0300

    moved login to auth service

--- client/src/app/shared/services/data.service.ts
+++ client/src/app/shared/services/data.service.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { Login } from '../../login/login.model';
 import { environment } from 'src/environments/environment';
 import { catchError } from 'rxjs/operators';
 import { Register } from '../../register/register.model';
 import { UserAccount } from 'src/app/user-account/user-account.model';
 
-const LOGIN_URL = 'user/login';
 const REGISTER_URL = 'user/';
 const ACCOUNT_URL = 'user/getAccountDetails/'
 const VERIFICATION_URL = 'user/verifyEmail';
 @Injectable({
-
   providedIn: 'root',
 })
 export class DataService {
@@ -45,11 +42,11 @@ export class DataService {
       .pipe(catchError(this.handleError));
 
   }
-  public login = (loginObj: Login) => {
-    return this.http.get<string>(this.createCompleteRoute(LOGIN_URL, environment.baseURL),
-      { params: { email: loginObj.email, password: loginObj.password } })
-      .pipe(catchError(this.handleError));
-  }
+  // public login = (loginObj: Login) => {
+  //   return this.http.get<string>(this.createCompleteRoute(LOGIN_URL, environment.baseURL),
+  //     { params: { email: loginObj.email, password: loginObj.password } })
+  //     .pipe(catchError(this.handleError));
+  // }
   public verifyEmail = (email: string) => {
     return this.http.post<void>(this.createCompleteRoute(VERIFICATION_URL, environment.baseURL), email, this.generateHeaders())
       .pipe(catchError(this.handleError));
