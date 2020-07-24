09af0a1ba7f03680219d8b419487d4b5b973bd8c
estherRivka <estherivka1@gmail.com>
2020-07-24 11:05:16 +0300

    fixed routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { UserAccountComponent } from './user-account/user-account.component';
+import { AuthGuard } from './auth/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'user/:userFileId', component: UserAccountComponent, canActivate: [AuthGuard] },
+  { path: "", redirectTo: '/login', pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
