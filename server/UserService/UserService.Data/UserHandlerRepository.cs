efa0be663469609310c2e21ce71a1bae402889a9
Tzippy Freedman <tzippyfreedman1@gmail.com>
2020-07-26 12:24:39 +0300

    added commit transfer handler in user service

--- server/UserService/UserService.Data/UserHandlerRepository.cs
+++ server/UserService/UserService.Data/UserHandlerRepository.cs
@@ -10,18 +10,18 @@ using UserService.NServiceBus.Services.Interfaces;
 
 namespace UserService.Data
 {
-   public class UserAccountHandlerRepository :ICommitTransferHandlerRepository
+   public class UserHandlerRepository :IUserHandlerRepository
     { 
      private readonly UserDbContext _userDbContext;
     private readonly IMapper _mapper;
-    public UserAccountHandlerRepository(UserDbContext userDbContext, IMapper mapper)
+    public UserHandlerRepository(UserDbContext userDbContext, IMapper mapper)
     {
         _userDbContext = userDbContext;
         _mapper = mapper;
 
     }
 
-    public bool CheckBalance(Guid accountId, float amount)
+    public bool CheckBalance(Guid accountId, int amount)
     {
         Account user = _userDbContext.Accounts.Where(u => u.Id == accountId).FirstOrDefault();
         bool isBalanceOK = user.Balance >= amount ? true : false;
@@ -33,10 +33,8 @@ namespace UserService.Data
         return await _userDbContext.Accounts.AnyAsync(u => u.Id == accountId);
     }
 
-    public async Task Pull(Guid account, float amount)
+    public async Task Draw(Guid account, int amount)
     {
-        //include??
-        //User user=await _userDbContext.Users.Where(u => u.UserFile.Id == srcAccount).FirstOrDefaultAsync();
         Account userAccount = await _userDbContext.Accounts.Where(u => u.Id == account).FirstOrDefaultAsync();
         if (userAccount == null)
         {
@@ -44,14 +42,14 @@ namespace UserService.Data
         }
         else
         {
-            //userAccount.Balance -= amount;
-            //userAccount.UpdateDate = DateTime.Now;
-        }
+                userAccount.Balance -= amount;
+                //userAccount.UpdateDate = DateTime.Now;
+            }
         //    await _userDbContext.SaveChangesAsync();
 
     }
 
-    public async Task Push(Guid account, float amount)
+    public async Task Deposit(Guid account, int amount)
     {
         Account userAccount = await _userDbContext.Accounts.Where(u => u.Id == account).FirstOrDefaultAsync();
         if (userAccount == null)
@@ -60,10 +58,10 @@ namespace UserService.Data
         }
         else
         {
-            //userAccount.Balance += amount;
-            //userAccount.UpdateDate = DateTime.Now;
+                userAccount.Balance += amount;
+                //userAccount.UpdateDate = DateTime.Now;
 
-        }
+            }
 
         //    await _userDbContext.SaveChangesAsync();
     }
