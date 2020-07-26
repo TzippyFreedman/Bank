80bef4a9afb5a7960139b85e35dd7f457ed127f1
estherRivka <estherivka1@gmail.com>
2020-07-26 10:41:16 +0300

    added messages class library

---
+++ server/Shared/Messages/Events/ITransferAdded.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Messages.Events
+{
+    public interface ITransferAdded
+    {
+         Guid TransferId { get; set; }
+         Guid FromAccount { get; set; }
+         Guid ToAccount   { get; set; }
+         float Amount { get; set; }
+    }
+}
--- server/TransferService/TransferService.Services/Models/TransferModel.cs
+++ server/TransferService/TransferService.Services/Models/TransferModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,10 @@ namespace TransferService.Services.Models
 {
     public class TransferModel
     {
+        public Guid Id { get; set; }
+        public TransferStatus Status { get; set; }
+        public Guid FromAccount { get; set; }
+        public Guid ToAccount { get; set; }
+        public float Amount { get; set; }
     }
 }
