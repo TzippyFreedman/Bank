a345616815a67d0c4958884e770a53152be55747
Tzippy Freedman <tzippyfreedman1@gmail.com>
2020-07-26 13:35:49 +0300

    added migration for failureReason column to transfer table in db

--- server/TransferService/TransferService.Data/Entities/Transfer.cs
+++ server/TransferService/TransferService.Data/Entities/Transfer.cs
@@ -12,5 +12,8 @@ namespace TransferService.Data.Entities
         public Guid FromAccount { get; set; }
         public Guid ToAccount { get; set; }
         public int Amount { get; set; }
+        public string FailureReason { get; set; }
+
+
     }
 }
---
+++ server/TransferService/TransferService.NServiceBus.Services/Interfaces/ITransferHandlerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferService.NServiceBus.Services.Interfaces
+{
+    public interface ITransferHandlerRepository
+    {
+        Task UpdateTransferStatus(Guid transferId, TransferStatus transferStatus, string failureReason);
+    }
+}
