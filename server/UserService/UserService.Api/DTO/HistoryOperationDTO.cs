365ff4dc4b6a51a8a4878e582fba481c2e9ece1a
estherRivka <estherivka1@gmail.com>
2020-07-28 15:04:12 +0300

    added sort by option

--- server/UserService/UserService.Api/DTO/HistoryOperationDTO.cs
+++ server/UserService/UserService.Api/DTO/HistoryOperationDTO.cs
@@ -7,9 +7,9 @@ namespace UserService.Api.DTO
 {
     public class HistoryOperationDTO
     {
+        public Guid TransactionId { get; set; }
         public DateTime OperationTime { get; set; }
         public float Amount { get; set; }
-
         public float Balance { get; set; }
         public bool IsDebit { get; set; }
         public Guid AccountId { get; set; }
--- server/UserService/UserService.Data/OperationsHistoryRepository.cs
+++ server/UserService/UserService.Data/OperationsHistoryRepository.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Threading.Tasks;
 using UserService.Contract;
 using UserService.Contract.Models;
@@ -61,16 +63,32 @@ namespace UserService.Data
             }
 
             response.OperationsTotal = await operations.CountAsync();
+            SortField sortField = paginationParams.SortField;
 
-            List<HistoryOperation> operationList = await operations
-                .OrderByDescending(operation => operation.OperationTime)
+            List<HistoryOperation> operationList =  await operations
+                .OrderBy(sortField.ToString(),paginationParams.SortDirection
+                .ToString() == "Asc" ? false : true)
                 .Skip((paginationParams.PageIndex) * paginationParams.PageSize).Take(paginationParams.PageSize)
                 .ToListAsync();
 
             response.OperationList = _mapper.Map<List<HistoryOperationModel>>(operationList);
-
             return response;
+        }
+    }
 
+    public static class OrderByHelper
+    {
+        public static IOrderedQueryable<TEntity> OrderBy<TEntity>(this IQueryable<TEntity> source, string orderByProperty, bool desc)
+        {
+            string command = desc ? "OrderByDescending" : "OrderBy";
+            var type = typeof(TEntity);
+            var property = type.GetProperty(orderByProperty, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+            var parameter = Expression.Parameter(type, "p");
+            var propertyAccess = Expression.MakeMemberAccess(parameter, property);
+            var orderByExpression = Expression.Lambda(propertyAccess, parameter);
+            var resultExpression = Expression.Call(typeof(Queryable), command, new Type[] { type, property.PropertyType },
+                source.Expression, Expression.Quote(orderByExpression));
+            return (IOrderedQueryable<TEntity>)source.Provider.CreateQuery<TEntity>(resultExpression);
         }
     }
 }
