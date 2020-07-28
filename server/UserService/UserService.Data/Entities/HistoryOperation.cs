a95cf1dc1d24b94db641e47550ead106e0f694bc
estherRivka <estherivka1@gmail.com>
2020-07-28 11:03:49 +0300

    added history handler

---
+++ server/UserService/UserService.Data/Entities/HistoryOperation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace UserService.Data.Entities
+{
+    public class HistoryOperation
+    {
+        public Guid Id { get; set; }
+        public Guid AccountId { get; set; }
+        public Guid TransactionId { get; set; }
+        public bool IsCredit { get; set; }
+        public int TransactionAmount { get; set; }
+        public int Balance { get; set; }
+        public DateTime OperationTime { get; set; }
+
+    }
+}
